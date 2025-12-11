19483638ea5a0ed334185fb79d8accdacced906e
Akshay Pratap Singh <akshaypratapsingh1244@gmail.com>
2025-12-11 17:37:31 +0530

    Done

--- Controller/DataLogic.js
+++ Controller/DataLogic.js
@@ -4,7 +4,7 @@ const User = require("../Schema/Model");
 const XLSX = require("xlsx");
 const { sendMail } = require("../utils/mailer");
 
-// DataentryLogic - Create a single entry (check)
+// DataentryLogic - Create a single entry (ch
 const DataentryLogic = async (req, res) => {
   try {
     const {
